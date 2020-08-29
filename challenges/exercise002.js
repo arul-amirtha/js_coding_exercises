7940f1c688fc8169b50a9a582888abd547e6738e
Arul-Amirtha <arul.arulnathan@gmail.com>
2020-08-29 13:57:35 +0100

    Format document updation in exercise 1 and 2

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -5,19 +5,19 @@ function getFillings(sandwich) {
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  return person.city=='Manchester'
+  return person.city == 'Manchester'
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  if (people%40==0 ) return people/40
-  return Math.floor(people/40)+1
+  if (people % 40 == 0) return people / 40
+  return Math.floor(people / 40) + 1
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  
-  let countsheep = arr.reduce(function(n, currentValue) {
+
+  let countsheep = arr.reduce(function (n, currentValue) {
     return n + (currentValue == 'sheep');
   }, 0);
   return countsheep;
@@ -25,8 +25,8 @@ function countSheep(arr) {
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  return (person.address.postCode.trim().charAt(0).toUpperCase()=='M' && 
-     person.address.city.toUpperCase().trim()=='MANCHESTER')
+  return (person.address.postCode.trim().charAt(0).toUpperCase() == 'M' &&
+    person.address.city.toUpperCase().trim() == 'MANCHESTER')
 }
 
 module.exports = {
